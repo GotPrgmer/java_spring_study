c61dee61e33b89c769667977b4a9fd75e9d465f2
Gotprgmer <gofiction95@gmail.com>
2023-03-08 07:32:25 +0900

    h2 설치 및 실행

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -4,13 +4,18 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 //스프링처음에 뜰때(실행시킬때) 컨테이너 통이 생기면
 //MemberController객체 생성해서 컨테이너 통에 넣어둠
 @Controller
 public class MemberController {
 
-    private final  MemberService memberService;
+    private final MemberService memberService;
 
     //세터 주입은 나중에 해당 함수로 변경할 수 있기 때문에 권장하지 않음
 //    @Autowired
@@ -19,8 +24,28 @@ public class MemberController {
 //    }
 
     @Autowired
-    public MemberController(MemberService memberService){
+    public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
 
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
 }
