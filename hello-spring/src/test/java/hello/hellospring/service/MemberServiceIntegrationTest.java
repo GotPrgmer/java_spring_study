ad1d360ab1da8c0f90772b03cfe61be7a2909283
김민준 <gofiction95@gmail.com>
2023-03-08 17:11:23 +0900

    JdbcTemplate 강의 완료

---
+++ hello-spring/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,83 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest {
+
+    @Autowired
+    MemberService memberService;
+    //여기서 다른 객체로 Test를 하고있으니, 같은 객체로 테스트를 하는 코드로 변경시켜야한다.
+    @Autowired
+    MemberRepository memberRepository = new MemoryMemberRepository();
+
+    //테스트를 실행할 때마다 각각 실행이 된다.
+//    @BeforeEach
+//    public void beforeEach(){
+//        memberRepository = new MemoryMemberRepository();
+//        //외부에서 memberRepository를 MemberService에 넣어줌(dependency injection)
+//        memberService = new MemberService(memberRepository);
+//    }
+
+
+//    @AfterEach
+//    public void afterEach(){
+//        memberRepository.clearStore();
+//    }
+
+
+
+    @Test
+    void 회원가입() {
+        //given
+        Member member = new Member();
+        member.setName("spring");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        // then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+
+    }
+
+    @Test
+    public void 중복_회원_예외() {
+        //given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        //when
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+//        try{
+//            memberService.join(member2);
+//            fail();
+//
+//        }catch(IllegalStateException e){
+//            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.123");
+//        }
+
+        //then
+
+    }
+
+
+}
